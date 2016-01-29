6ca4cd7269b49ad79fefbc7edacf7ce80cd7d004
luisalfredomoctezuma <luisalfredomoctezuma@gmail.com>
2016-01-28 21:56:37 -0600

    D-H Mov

--- DH.m
+++ DH.m
@@ -1,14 +1,11 @@
 %% D-H
 function A=DH(teta,d,a,alfa)
 
-A=[cos(teta)    -cos(alfa)*sin(teta)    sin(alfa)*sin(teta)     a*cos(teta);
-   sin(teta)    cos(alfa)*cos(teta)     -sin(alfa)*cos(teta)    a*sin(teta);
-   0            sin(alfa)               cos(alfa)               d;
+
+A=[cos(teta)    -sin(teta)    0     a;
+   cos(alfa)*sin(teta)	cos(alfa)*cos(teta) -sin(alfa)	-d*sin(alfa);
+   sin(alfa)*sin(teta)	sin(alfa)*sin(teta) cos(alfa)	d*cos(alfa);
    0            0                       0                       1];
 
 
-% Articulacion  Q   d   a   A
-%   1           Q1  d1  0   0
-%   2           90  d2  0   90
-%   3           0   d3  0   0
-%   4           Q4  d4  0   0
+
--- matrices.m
+++ matrices.m
@@ -1,13 +1,10 @@
 global RotacionZ RotacionX RotacionXX TrasEslabon1 TrasEslabon2 TrasEslabon0;
 global teta1 teta2 teta3 CDM;
 global Eslabon0 Eslabon1 Eslabon2;
-% teta1
-% teta2
-% teta3
-% disp('---')
 set(handles.text2,'string',teta1)
 set(handles.text3,'string',teta2)
 set(handles.text4,'string',teta3)    
+
 RotacionZ=[cosd(teta1) -sind(teta1) 0 0;
           sind(teta1)  cosd(teta1)  0 0;
           0            0            1 0;
@@ -30,7 +27,7 @@ TrasEslabon1=[1,0,0,0;
               0,0,0,1];          
 TrasEslabon2=[1,0,0,0;
               0,1,0,0;
-              0,0,0,7;
+              0,0,1,7;
               0,0,0,1];          
 E0=RotacionZ*Eslabon0;
 E1=(RotacionZ*TrasEslabon0)*RotacionX*TrasEslabon1;
@@ -49,32 +46,39 @@ line([E0(1) E1(1,4)],[E0(2) E1(2,4)],[E0(3) E1(3,4)],'color','r','LineWidth',20)
 hold on
 line([E1(1,4) E2(1,4)],[E1(2,4) E2(2,4)],[E1(3,4) E2(3,4)],'color','black','LineWidth',10)
 grafo;
-% Cinematica Directa con Matrices
+
+%% Cinematica Directa con Matrices
 CDM=E2(:,4);
 set(handles.text8,'string',CDM(1))
 set(handles.text9,'string',CDM(2))
 set(handles.text10,'string',CDM(3))
 
-%% Cinematica Directa M-Geometrico
+%% M-Geometrico
 % disp('--')
-% z=11*cosd(teta2)+7*cosd(teta2+teta3);
-% y=11*sind(teta2)+7*sind(teta2+teta3);
-% r=sqrt((z^2)+(y^2));
-% % Cuanto giro en Z?  
-% x=r*cosd(teta1);
-% xx=r*sind(teta1);
-% 
-% set(handles.text13,'string',CDM(1))
-% set(handles.text14,'string',CDM(2))
-% set(handles.text15,'string',z)
+z=11*cosd(teta2)+9*cosd(teta2+teta3);
+y=11*sind(teta2)+9*sind(teta2+teta3);
+r=sqrt((z^2)+(y^2));
+% Cuanto giro en Z?  
+x=r*cosd(teta1);
+xx=r*sind(teta1);
+
+set(handles.text13,'string',CDM(1))
+set(handles.text14,'string',CDM(2))
+set(handles.text15,'string',CDM(3))
+
 
 
 %% D-H
-% T =[                       cosd(teta1)*cosd(teta3) - sind(teta1)*sind(teta3),                     - cosd(teta1)*sind(teta3) - cosd(teta3)*sind(teta1),           0,              0;
-% cosd(teta1)*cosd(teta2)*sind(teta3) + cosd(teta2)*cosd(teta3)*sind(teta1), cosd(teta1)*cosd(teta2)*cosd(teta3) - cosd(teta2)*sind(teta1)*sind(teta3), -sind(teta2), -18*sind(teta2);
-% cosd(teta3)*sind(teta1)*sind(teta2) + sind(teta1)*sind(teta2)*sind(teta3), cosd(teta3)*sind(teta1)*sind(teta2) - sind(teta1)*sind(teta2)*sind(teta3),  cosd(teta2),  18*cosd(teta2);
-% 0,                                                                   0,           0,              1]
- 
+T=[ cosd(teta1), sind(teta1)*sind(teta2)*sind(teta3) - cosd(teta2)*cosd(teta3)*sind(teta1),   cosd(teta2)*sind(teta1)*sind(teta3) + cosd(teta3)*sind(teta1)*sind(teta2),   11*sind(teta1)*sind(teta2) + 7*cosd(teta2)*sind(teta1)*sind(teta3) + 7*cosd(teta3)*sind(teta1)*sind(teta2);
+	 sind(teta1), cosd(teta1)*cosd(teta2)*cosd(teta3) - cosd(teta1)*sind(teta2)*sind(teta3), - cosd(teta1)*cosd(teta2)*sind(teta3) - cosd(teta1)*cosd(teta3)*sind(teta2), - 11*cosd(teta1)*sind(teta2) - 7*cosd(teta1)*cosd(teta2)*sind(teta3) - 7*cosd(teta1)*cosd(teta3)*sind(teta2);
+	       0,                       cosd(teta2)*sind(teta3) + cosd(teta3)*sind(teta2),                         cosd(teta2)*cosd(teta3) - sind(teta2)*sind(teta3),                                11*cosd(teta2) + 7*cosd(teta2)*cosd(teta3) - 7*sind(teta2)*sind(teta3) + 2;
+	       0,                                                                   0,                                                                     0,                                                                                                    1]       
 
-%%
+set(handles.text17,'string',T(1,4))
+set(handles.text18,'string',T(2,4))
+set(handles.text19,'string',T(3,4))
+
+
+
+
 
